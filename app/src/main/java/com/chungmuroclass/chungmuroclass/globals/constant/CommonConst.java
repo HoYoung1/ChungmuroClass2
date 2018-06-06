dfafe82d5715ce58ea822d2deddf150b0ef16825
HoYoung1 <empire1641@gmail.com>
2018-06-06 14:13:37 +0900

    delete access key

--- app/src/main/java/com/chungmuroclass/chungmuroclass/globals/constant/CommonConst.java
+++ app/src/main/java/com/chungmuroclass/chungmuroclass/globals/constant/CommonConst.java
@@ -5,6 +5,6 @@ package com.chungmuroclass.chungmuroclass.globals.constant;
  */
 
 public class CommonConst {
-    public final static String S3KEY = "AKIAJ6PNICAXLR3VQFYA";
-    public final static String S3SECRET = "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";
+    public final static String S3KEY = "";
+    public final static String S3SECRET = "";
 }
